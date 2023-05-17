60cd466294a249c9417a06738e3aaab1fb329da7
Shane Harper <shane@shaneharper.net>
2023-05-17 20:06:53 +1000

    Text following "&@REM " will now be shown as a comment.
    Replace four "syn match dosbatchComment" lines with two.

--- tests/comments.bat
+++ tests/comments.bat
@@ -1,8 +1,11 @@
 dir  &REM Comment
-@REM ^    ^^^^^^^ This should be highlighted as "dosbatchComment" or "Comment". (This originally failed - the syntax highlighting used to require a space between '&' and "REM".)
+@REM ^    ^^^^^^^ This should be highlighted as "dosbatchComment". (This originally failed - it'd work with a space between '&' and 'REM' but not without.)
 @REM |
-@REM ---- This should be highlighted as "dosbatchOperator" or "Operator".
+@REM ---- This should be highlighted as "dosbatchOperator".
 
 
+dir  &@REM This should be highlighted as "dosbatchComment".
 
-dir  &@REM This should be highlighted as "dosbatchComment" or "Comment". (It'd be rare for '@' to be used immediately after '&'?)
+REM Comment
+  REM Comment
+  @REM Comment
